b4611667d38a847c4d820504a898006063ac117d
Lunarixus <Nathan@lunarixus.dev>
2024-01-08 23:17:38 +0000

    Main: Implement Skeleton Classes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,44 @@
+// Create a static data class for Students
+data class Student (
+    val id: Int,
+    val name: String,
+    val course: String,
+    val avgMarks: Double
+)
+
+class UniversitySystem(private val Students: MutableList<Student> = mutableListOf())
+{
+    fun addStudent(Student: Student)
+    {
+
+    }
+
+    fun removeStudentById(StudentId: Int)
+    {
+
+    }
+
+    fun queryByName(StudentName: String)
+    {
+
+    }
+
+    fun queryById(StudentId: Int)
+    {
+
+    }
+
+    fun queryByCourse(CourseName: String)
+    {
+
+    }
+
+    fun queryByAvgMarks(AvgMarks: Double)
+    {
+
+    }
+}
+
 fun main() {
-    println("Hello, World!")
+    println("CI505 - Functional Programming")
 }
