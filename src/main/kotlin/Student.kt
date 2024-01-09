d028437c880db55c684c4c4c8bc570b0cf523933
Lunarixus <Nathan@lunarixus.dev>
2024-01-09 03:40:07 +0000

    Student: Add Comments

--- src/main/kotlin/Student.kt
+++ src/main/kotlin/Student.kt
@@ -1,8 +1,8 @@
 
 // Create a static data class for Students
 data class Student(
-    val id: Int,
-    val name: String,
-    val course: String,
-    val avgMarks: Double
+    val id: Int, // Student ID
+    val name: String, // Student Name
+    val course: String, // Enrolled Course
+    val avgMarks: Double // Average Course Marks
 )
