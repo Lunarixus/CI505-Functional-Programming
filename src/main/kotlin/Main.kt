7fdbc3f48a798fdb72b34768a0afd1ec81181c93
Lunarixus <Nathan@lunarixus.dev>
2024-01-09 00:02:30 +0000

    Main: Implement Adding and Removing a student

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,16 +6,16 @@ data class Student (
     val avgMarks: Double
 )
 
-class UniversitySystem(private val Students: MutableList<Student> = mutableListOf())
+class UniversitySystem(val Students: List<Student> = mutableListOf())
 {
-    fun addStudent(Student: Student)
+    fun addStudent(student: Student): UniversitySystem
     {
-
+        return UniversitySystem(Students + student) // Add a Student
     }
 
-    fun removeStudentById(StudentId: Int)
+    fun removeStudentById(StudentId: Int): UniversitySystem
     {
-
+        return UniversitySystem(Students.filterNot { it.id == StudentId })
     }
 
     fun queryByName(StudentName: String)
@@ -41,4 +41,22 @@ class UniversitySystem(private val Students: MutableList<Student> = mutableListO
 
 fun main() {
     println("CI505 - Functional Programming")
+
+    // Create an instance of the UniversitySystem class
+    val universityregister = UniversitySystem()
+
+    // Create an example student
+    val example_student = Student(0, "Mark", "Computer Science", 65.7)
+
+    // Add the student to the register
+    var updatedRegister = universityregister.addStudent(example_student)
+
+    // Print every student in the register
+    updatedRegister.Students.forEach { println(it) }
+
+    // Remove the student from the register
+    updatedRegister = updatedRegister.removeStudentById(0)
+
+    // Print every student in the register again
+    updatedRegister.Students.forEach { println(it) }
 }
