e3ca3aa993d9df35345d63d3f57862b5b514eccc
Lunarixus <Nathan@lunarixus.dev>
2024-01-09 03:40:33 +0000

    UniversitySystem: Add Comments

--- src/main/kotlin/UniversitySystem.kt
+++ src/main/kotlin/UniversitySystem.kt
@@ -1,25 +1,25 @@
 class UniversitySystem(val students: List<Student> = mutableListOf()) {
-    fun addStudent(student: Student): UniversitySystem {
+    fun addStudent(student: Student): UniversitySystem { // Add a Student
         return UniversitySystem(students + student)
     }
 
-    fun removeStudentById(studentId: Int): UniversitySystem {
+    fun removeStudentById(studentId: Int): UniversitySystem { // Remove a student by ID
         return UniversitySystem(students.filterNot { it.id == studentId })
     }
 
-    fun queryByName(studentName: String): UniversitySystem {
+    fun queryByName(studentName: String): UniversitySystem { // Query a Student by name
         return UniversitySystem(students.filter { it.name == studentName })
     }
 
-    fun queryById(studentId: Int): UniversitySystem {
+    fun queryById(studentId: Int): UniversitySystem { // Query a Student by their ID
         return UniversitySystem(students.filter { it.id == studentId })
     }
 
-    fun queryByCourse(courseName: String): UniversitySystem {
+    fun queryByCourse(courseName: String): UniversitySystem { // Query Students by course
         return UniversitySystem(students.filter { it.course == courseName })
     }
 
-    fun queryByAvgMarks(avgMarks: Double): UniversitySystem {
-        return UniversitySystem(students.filter { it.avgMarks >= avgMarks })
+    fun queryByAvgMarks(avgMarks: Double): UniversitySystem { // Query Students by average marks (bigger than entered value)
+        return UniversitySystem(students.filter { it.avgMarks >= avgMarks }) // Bigger than or equal to
     }
 }
